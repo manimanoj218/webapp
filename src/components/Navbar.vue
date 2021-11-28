6656dca00f67acfa85f5386e993f3b46f885fe8a
manoj <manoj.r@vigocare.com>
2021-11-28 13:42:32 +0530

    created initial set up for online webapp

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-menu
+    :default-active="'/'"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    :router="true"
+    active-text-color="#ffd04b"
+  >
+    <el-menu-item index="/">Home</el-menu-item>   
+    <el-menu-item index="/about">About</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      activeIndex2: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
